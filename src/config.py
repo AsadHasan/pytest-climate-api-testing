04faf82a06a79d962c03ab9eb5341389bfeeed99
AsadHasan <asadhasan180@hotmail.com>
2020-08-25 12:33:19 +0100

    basic test for climate api

---
+++ src/config.py
@@ -0,0 +1,3 @@
+"""Config file to hold global variables
+    """
+BASE_URL = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country"
---
+++ src/tests/test_api.py
@@ -0,0 +1,37 @@
+"""Tests for World Bank's climate API
+"""
+
+import pytest
+import requests
+from requests.models import Response
+
+from config import BASE_URL
+
+
+@pytest.mark.parametrize(
+    "type,gcm,var,start,end,country",
+    [
+        ("mavg", "bccr_bcm2_0", "pr", 2020, 2039, "gbr"),
+        ("annualavg", "cccma_cgcm3_1", "tas", 1980, 1999, "pak"),
+        ("manom", "gfdl_cm2_1", "pr", 2060, 2079, "ind"),
+        ("annualanom", "mpi_echam5", "tas", 2080, 2099, "usa"),
+    ],
+)
+def test_climate_api(
+    type: str, gcm: str, var: str, start: int, end: int, country: str
+) -> None:
+    """Failure of this test implies that API does not
+    return correct response for valid parameter permutations
+    """
+    path: str = f"{type}/{gcm}/{var}/{start}/{end}/{country}"
+    url: str = f"{BASE_URL}/{path}"
+    response: Response = requests.get(url)
+    response.raise_for_status()
+    json_response: dict = response.json()
+    assert json_response != "Invalid request parameters"
+    assert len(json_response) > 0
+    for item in json_response:
+        assert item["gcm"] == gcm
+        assert item["variable"] == var
+        assert item["fromYear"] == start
+        assert item["toYear"] == end
